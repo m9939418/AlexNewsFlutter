7ffbbd10be13fa5bb8b4a584e35774d5d1edb848
AlexYang <m9939418@gmail.com>
2024-01-15 13:30:55 +0800

    #17 add Pagination Widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:alex_news_flutter/consts/vars.dart';
 import 'package:alex_news_flutter/widgets/drawer_widget.dart';
 import 'package:alex_news_flutter/widgets/tabs_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +13,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   var newsType = NewsType.allNews;
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -61,9 +63,76 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ],
             ),
-          )
+          ),
+          newsType == NewsType.topTrending
+              ? Container()
+              : SizedBox(
+                  height: kBottomNavigationBarHeight,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      paginationButton(
+                          function: () {
+                            setState(() {
+                              if (currentPageIndex == 0) return;
+                              currentPageIndex -= 1;
+                            });
+                          },
+                          text: 'prev'),
+                      Flexible(
+                        flex: 2,
+                        child: ListView.builder(
+                            itemCount: 5,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Material(
+                                  color: currentPageIndex == index
+                                      ? Colors.blue
+                                      : Theme.of(context).cardColor,
+                                  child: InkWell(
+                                      onTap: () {
+                                        setState(() {
+                                          currentPageIndex = index;
+                                        });
+                                      },
+                                      child: Center(
+                                          child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text("${index + 1}"),
+                                      ))),
+                                ),
+                              );
+                            }),
+                      ),
+                      paginationButton(
+                          function: () {
+                            setState(() {
+                              if (currentPageIndex == 4) return;
+                              currentPageIndex += 1;
+                            });
+                          },
+                          text: 'next'),
+                    ],
+                  ),
+                ),
         ],
       ),
+    );
+  }
+
+  Widget paginationButton({required Function function, required String text}) {
+    return ElevatedButton(
+      onPressed: () {
+        function();
+      },
+      child: Text(text),
+      style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+          padding: const EdgeInsets.all(6),
+          textStyle:
+              const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
     );
   }
 }
