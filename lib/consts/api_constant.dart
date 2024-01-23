c7d2fa7f3a13735de110d31ddf5fe9663906b6d2
AlexYang <m9939418@gmail.com>
2024-01-24 01:37:54 +0800

    #62 setup Firebase Project and create Realtime Database

--- lib/consts/api_constant.dart
+++ lib/consts/api_constant.dart
@@ -1,3 +1,4 @@
+const String BASEURL = "newsapi.org";
+const String API_KEY = "2bd20a8e990348838b760332bbe293df";
 
-String BASEURL = "newsapi.org";
-String API_KEY = "2bd20a8e990348838b760332bbe293df";
+const String BASEURL_FIREBASE = "alexnewsflutter-default-rtdb.firebaseio.com";
